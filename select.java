020c6e77aebde70dac47507a56b3d736e007bb4c
Devendra Pandey <25032tjf97devendrakumar@viva-technology.org>
2026-09-12 20:48:01 +0530

    Add DB class for PostgreSQL connection and data retrieval
    Implement database connection and query execution.

---
+++ select.java
@@ -0,0 +1,47 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+class DB{
+    public static Connection getConnection(){
+        String url = "jdbc:postgresql://localhost:5432/Demo";
+        String username = "postgres";
+        String pwd = "kp724";
+        Connection con = null;
+        try{
+            con = DriverManager.getConnection(url,username,pwd);
+
+              PreparedStatement ps = con.prepareStatement("select * from demo");
+              ResultSet rs = ps.executeQuery();
+              while(rs.next()){
+                    System.out.println(rs.getInt(1));
+                    System.out.println(rs.getString(2));
+                    System.out.println(rs.getInt(3));
+              }
+          ps.close();
+              
+
+
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return con;
+    }
+}
+class Main{
+    public static void main(String[] args) {
+        Connection con = DB.getConnection();
+        if(con != null){
+            System.out.println("Connected");
+        }
+        try{
+            con.close();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        
+    }
+}
