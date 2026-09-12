18ec80cfb2ea77c7b7e37e10134bb6320c6cb6b6
Devendra Pandey <25032tjf97devendrakumar@viva-technology.org>
2026-09-12 21:21:22 +0530

    Implement database connection and update functionality
    Establish a connection to the PostgreSQL database and update a record in the 'demo' table.

---
+++ update.java
@@ -0,0 +1,74 @@
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Scanner;
+
+class DB{
+    
+    public static Connection getConnection(){
+        Scanner sc = new Scanner(System.in);
+        String url = "jdbc:postgresql://localhost:5432/Demo";
+        String username = "postgres";
+        String pwd = "kp724";
+        Connection con = null;
+        try{
+            con = DriverManager.getConnection(url,username,pwd);
+//            PreparedStatement ps = con.prepareStatement("insert into demo values(?,?,?)");
+//            ps.setInt(1, 5);
+//            ps.setString(2, "Rohit");
+//            ps.setInt(3, 77);
+//            ps.executeUpdate();
+//            ps.setInt(1, 7);
+//            ps.setString(2, "Navin");
+//            ps.setInt(3, 46);
+//            ps.executeUpdate();
+//            ps.close();
+
+    
+//            System.out.println("Enter Id: ");
+//            int inputId = sc.nextInt();
+//
+//            PreparedStatement ps = con.prepareStatement("select * from demo where id = " +inputId );
+//            ResultSet rs = ps.executeQuery();
+//
+//            while(rs.next()){
+//                  System.out.println(rs.getInt(1));
+//                  System.out.println(rs.getString(2));
+//                  System.out.println(rs.getInt(3));
+//            }
+//            ps.close();
+              
+
+            
+            String sql = "Update demo set marks = ? where id = ?;";
+            PreparedStatement ps = con.prepareStatement(sql);
+            ps.setInt(1, 55);
+            ps.setInt(2, 7);
+            ps.executeUpdate();
+
+
+
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        return con;
+    }
+}
+class Main{
+    public static void main(String[] args) {
+        
+        
+        Connection con = DB.getConnection();
+        if(con != null){
+            System.out.println("Connected");
+        }
+        try{
+            con.close();
+        }catch(Exception e){
+            e.printStackTrace();
+        }
+        
+    }
+}
