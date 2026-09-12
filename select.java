61f448ce74984c30ca5a60c9815bf782d617d7b1
Devendra Pandey <25032tjf97devendrakumar@viva-technology.org>
2026-09-12 20:52:59 +0530

    Modify SQL query to use parameterized ID selection

--- select.java
+++ select.java
@@ -12,15 +12,33 @@ class DB{
         Connection con = null;
         try{
             con = DriverManager.getConnection(url,username,pwd);
+//            PreparedStatement ps = con.prepareStatement("insert into demo values(?,?,?)");
+//            ps.setInt(1, 5);
+//            ps.setString(2, "Rohit");
+//            ps.setInt(3, 77);
+//            ps.executeUpdate();
+//            ps.setInt(1, 7);
+//            ps.setString(2, "Navin");
+//            ps.setInt(3, 46);
+//            ps.executeUpdate();
+//            ps.close();
+              
 
-              PreparedStatement ps = con.prepareStatement("select * from demo");
+              PreparedStatement ps = con.prepareStatement("select * from demo where id = ?");
+              
+              for(int i=1; i<3; i++)
+              {
+              ps.setInt(1, i);
               ResultSet rs = ps.executeQuery();
+              
               while(rs.next()){
                     System.out.println(rs.getInt(1));
                     System.out.println(rs.getString(2));
                     System.out.println(rs.getInt(3));
               }
-          ps.close();
+              }
+              ps.close();
+              
               
 
 
